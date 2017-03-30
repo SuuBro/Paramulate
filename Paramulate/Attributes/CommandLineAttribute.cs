86b2b606063e6990abf16b9f0d049cacfad0e6ef
SuuBro <J.Subra@gmail.com>
2017-03-30 10:27:16 +0100

    Add CommandLineAttribute

---
+++ Paramulate/Attributes/CommandLineAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Paramulate.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public class CommandLineAttribute
+    {
+        public string PathToDeeperKey { get; }
+        public string ShortReferenceKey { get; }
+        public string ReferenceKey { get; }
+        public string HelpText { get; }
+
+        public CommandLineAttribute(string helpText)
+        {
+            HelpText = helpText;
+        }
+
+        public CommandLineAttribute(string referenceKey, string helpText)
+            : this(helpText)
+        {
+            ReferenceKey = referenceKey;
+        }
+
+        public CommandLineAttribute(string shortReferenceKey, string referenceKey, string helpText)
+            : this(referenceKey, helpText)
+        {
+            ShortReferenceKey = shortReferenceKey;
+        }
+
+        public CommandLineAttribute(string pathToDeeperKey, string shortReferenceKey,
+            string referenceKey, string helpText)
+            : this(shortReferenceKey, referenceKey, helpText)
+        {
+            PathToDeeperKey = pathToDeeperKey;
+        }
+    }
+}
